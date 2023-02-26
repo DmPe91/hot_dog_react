e9a29734bb9312d5a74251e81360d97ed9266d69
DmPe91 <dmper67@gmail.com>
2023-02-26 21:50:27 +0300

    scss разбитие

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { Routes, Route } from "react-router-dom";
-import "./style.scss";
+import "./scss/style.scss";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import EmptyCart from "./pages/EmptyCart";
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../style.scss";
+//import "../style.scss";
 
 import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
