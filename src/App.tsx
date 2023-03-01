a273c2aa95a1b15958f0ead0ecad460d5439bc6e
DmPe91 <dmper67@gmail.com>
2023-03-01 14:22:32 +0300

    hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./scss/style.scss";
 import Header from "./components/Header";
 import Home from "./pages/Home";
@@ -11,20 +11,19 @@ function App() {
   return (
     <div className="wrapper">
       <Header />
-      <HashRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/cart"
-            element={
-              <React.Suspense fallback={<div>Загрузка корзины...</div>}>
-                <Cart />
-              </React.Suspense>
-            }
-          />
-          <Route path="*" element={<EmptyCart />} />
-        </Routes>
-      </HashRouter>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/cart"
+          element={
+            <React.Suspense fallback={<div>Загрузка корзины...</div>}>
+              <Cart />
+            </React.Suspense>
+          }
+        />
+        <Route path="*" element={<EmptyCart />} />
+      </Routes>
     </div>
   );
 }
