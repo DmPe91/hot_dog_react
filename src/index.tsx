d09dae60b69763a026732d9dd64df590049c8d64
DmPe91 <105983049+DmPe91@users.noreply.github.com>
2023-03-01 18:03:36 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const rootElem = document.getElementById("root");
 if (rootElem) {
   const root = ReactDOM.createRoot(rootElem);
   root.render(
-    <BrowserRouter>
+    <BrowserRouter basename="/">
     <Provider store={store}>
      <App />
       </Provider>
