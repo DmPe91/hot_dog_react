ae90bed2bb3d0bf98aab41186ab309b7ff47ed4f
DmPe91 <105983049+DmPe91@users.noreply.github.com>
2023-03-01 15:20:05 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -12,10 +12,10 @@ const rootElem = document.getElementById("root");
 if (rootElem) {
   const root = ReactDOM.createRoot(rootElem);
   root.render(
+    <Provider store={store}>
     <HashRouter basename="/>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </HashRouter>
+      <App />
+    </HashRouter>  
+     </Provider>
   );
 }
